64aa01ef5df6f60bc53d9e3532ef6f708d2782e3
Romain <RomainCapo@users.noreply.github.com>
2019-10-04 16:54:42 +0200

    remove test route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function() {
-  return view('test');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
