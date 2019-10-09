c6bbff5cba166327accd6b02521dee5415cd3934
Capocasale Romain <romain.capocasa@he-arc.ch>
2019-10-09 17:57:12 +0200

    add google map on form

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -2,32 +2,35 @@
 
 @section('content')
 <div class="container">
-  <h1>Create a company account</h1>
-  <br>
+  <div id="createCompanyAccount">
+    <h1>Create a company account</h1>
+      {{ Form::open(['route' => 'home']) }}
+      <div id="companyRegisterPart1" style="display:none;">
+        {{ Form::label('email', 'E-Mail Address : ') }}
+        {{ Form::email('email', null, ['class' => 'form-control']) }}
 
-{{ Form::open(['route' => 'home']) }}
-<div id="companyRegisterPart1">
-  {{ Form::label('email', 'E-Mail Address : ') }}
-  {{ Form::email('email', null, ['class' => 'form-control']) }}
-  <br>
-  {{ Form::label('firstName', 'First Name : ') }}
-  {{ Form::text('firstName', null, ['class' => 'form-control']) }}
-  <br>
-  {{ Form::label('lastName', 'Last Name : ') }}
-  {{ Form::text('lastName', null, ['class' => 'form-control']) }}
-  <br>
-  {{ Form::label('companyName', 'Company Name : ') }}
-  {{ Form::text('companyName', null, ['class' => 'form-control']) }}
-  <br>
-  {{ Form::label('password', 'Password : ') }}
-  {{ Form::password('password', ['class' => 'form-control']) }}
-  <br>
-  {{ Form::label('confirmPassword', 'Confirm Password : ') }}
-  {{ Form::password('confirmPassword', ['class' => 'form-control']) }}
-  <br>
-  <a href="#" id="btnNextStep1" class="btn btn-dark btn-block" role="button">Next Step (1/3)</a>
-</div>
+        {{ Form::label('firstName', 'First Name : ') }}
+        {{ Form::text('firstName', null, ['class' => 'form-control']) }}
 
+        {{ Form::label('lastName', 'Last Name : ') }}
+        {{ Form::text('lastName', null, ['class' => 'form-control']) }}
+
+        {{ Form::label('password', 'Password : ') }}
+        {{ Form::password('password', ['class' => 'form-control']) }}
+
+        {{ Form::label('confirmPassword', 'Confirm Password : ') }}
+        {{ Form::password('confirmPassword', ['class' => 'form-control']) }}
+
+        <a href="#" id="btnNextStep1" class="btn btn-dark btn-block" role="button">Next Step (1/3)</a>
+      </div>
+      <div id="companyRegisterPart2">
+        {{ Form::label('companyName', 'Company Name : ') }}
+        {{ Form::text('companyName', null, ['class' => 'form-control']) }}
+
+        <div id="map" style="height:400px; width:100%;"></div>
+      </div>
   {{ Form::close() }}
+</div>
+  
 
 @endsection
