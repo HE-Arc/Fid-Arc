e3f4c30e6aad5e9aa15e01249e4a036df1495c5a
Capocasale Romain <romain.capocasa@he-arc.ch>
2019-10-09 17:57:12 +0200

    add google map on form

--- public/js/main.js
+++ public/js/main.js
@@ -99,11 +99,23 @@ function placeMarker(location) {
 }
 =======
 // Dom is loaded
-
 (function() {
   document.getElementById("btnNextStep1").addEventListener("click", function(e){
     e.target.parentElement.style.display = 'none'
   });
 
+
+
 })();
+<<<<<<< HEAD
 >>>>>>> add begin of companies register form
+=======
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 46.991740, lng: 6.928846},
+    zoom: 10
+  });
+}
+>>>>>>> add google map on form
