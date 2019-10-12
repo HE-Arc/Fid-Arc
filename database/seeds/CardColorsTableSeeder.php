ded12878ee74a32d43e55f7b9f1b63959efa7c85
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-10-12 15:55:25 +0200

    add companies migration, card_colors migration and card_colors seeder

---
+++ database/seeds/CardColorsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CardColorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('card_colors')->insert([
+          ['color' => 'red'],
+          ['color' => 'blue'],
+          ['color' => 'green'],
+          ['color' => 'yellow'],
+          ['color' => 'black'],
+          ['color' => 'white']]);
+    }
+}
