1441947b2b4aa0d820fae3776438ab1f265b7271
Alexandre Bianchi, Scanix <alexandre.bianchi@nectoria.com>
2019-10-31 15:19:20 +0000

    Force https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+	if(config('app.env') === 'production')
+	{
+		\URL::forceScheme('https');
+	}
     }
 }
