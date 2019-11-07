680bb565ec6297c129b28f107261a3cfa022e8d2
Vincent Moulin <vincent.moulin@he-arc.ch>
2019-11-07 15:04:53 +0100

    added message for empty fidelity cards

--- public/js/getPersonnalInfosViaApi.js
+++ public/js/getPersonnalInfosViaApi.js
@@ -6,19 +6,29 @@ fetch('http://127.0.0.1:8000/public/api/fidelityCards/9').then(
         }
 
         response.json().then(function(data) {
-            data.data.forEach(element => {
-            let html = "<div class='card mb-3 color_card_" + element.card_color_id + "'>"
-            html += "<div class='card-body'>"
-            html += "<h3 class='card-title mb-0'>" + element.company_name + "</h3>"
-            html += "<p><strong>" + element.message_to_user + "</strong></p>"
-            html += "<p class='mb-0'>Your fidelity points is <strong>" + element.current_number_of_points + "/" + element.total_number_of_points +"</strong></p>"
-            html += "</div>"
-            html += "</div>"
 
-            document.getElementById("loader-fidelitycards").style.display = "none"; 
-            document.getElementById("fidelitycards").innerHTML += html
-            });
-        });
+            var element = document.getElementById("loader-fidelitycards");
+            element.parentNode.removeChild(element);
+
+            if(data.data.length == 0)
+            {
+                document.getElementById("fidelitycards").innerHTML = "<p>You don't have any fidelity cards...</p>"
+            }
+            else
+            {
+                data.data.forEach(element => {
+                    let html = "<div class='card mb-3 color_card_" + element.card_color_id + "'>"
+                    html += "<div class='card-body'>"
+                    html += "<h3 class='card-title mb-0'>" + element.company_name + "</h3>"
+                    html += "<p><strong>" + element.message_to_user + "</strong></p>"
+                    html += "<p class='mb-0'>Your fidelity points is <strong>" + element.current_number_of_points + "/" + element.total_number_of_points +"</strong></p>"
+                    html += "</div>"
+                    html += "</div>"
+                    document.getElementById("fidelitycards").innerHTML += html
+                })
+            }
+            
+        })
     }
 ).catch(function(err) {
     console.log('Fetch Error : ', err)
