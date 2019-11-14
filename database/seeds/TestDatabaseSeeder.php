a697924c678f987d91495e9b7d31881e4fa1bc3e
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2019-11-14 14:28:49 +0100

    Create TestDatabaseSeeder for debug purpose

---
+++ database/seeds/TestDatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TestDatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset table for test
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        App\CardColor::truncate();
+        DB::table('roles')->truncate();
+        App\User::truncate();
+        App\Company::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $this->call(CardColorsTableSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(CompaniesTestSeeder::class);
+    }
+}
