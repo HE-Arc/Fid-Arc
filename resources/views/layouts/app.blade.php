ce94c710dd6cbbcf65e8e795d0ad99341184e937
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2019-12-12 17:18:41 +0100

    Simplify navbar when logged

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,27 +62,15 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('profile') }}">
-                                    {{ __('Profile') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
+                        <li class="nav-item {{ (request()->is('*Profile')) ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('profile') }}">{{ __('Profile') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"  href="{{ route('logout') }}">{{ __('Logout') }}</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                  style="display: none;">
+                                @csrf
+                            </form>
                         </li>
                     @endguest
                 </ul>
