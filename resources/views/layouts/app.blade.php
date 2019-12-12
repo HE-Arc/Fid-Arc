10d64c11bde3743b5393b884817ebed10a1bb183
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2019-12-12 17:45:05 +0100

    Add basic footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/main.js') }}" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+    <script src="https://kit.fontawesome.com/24fb1f2666.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -66,7 +67,9 @@
                             <a class="nav-link" href="{{ route('profile') }}">{{ __('Profile') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"  href="{{ route('logout') }}">{{ __('Logout') }}</a>
+                            <a class="nav-link"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                               href="{{ route('logout') }}">{{ __('Logout') }}</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                   style="display: none;">
                                 @csrf
@@ -81,10 +84,39 @@
     <main>
         @yield('content')
     </main>
-    <footer>
-        <div class="container-fluid">
-            <div class="col-12">
-                <small>Copyright &copy; 2019, Fid-Arc</small>
+    <footer class="mt-5 pt-5 pb-5 footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-xs-12 about-company">
+                    <h2>Heading</h2>
+                    <p class="pr-5 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac
+                        ante mollis quam tristique convallis </p>
+                    <p><a href="#"><i class="fab fa-facebook-square mr-1"></i></a><a href="#"><i class="fab fa-linkedin"></i></a></p>
+                </div>
+                <div class="col-lg-3 col-xs-12 links">
+                    <h4 class="mt-lg-0 mt-sm-3">Links</h4>
+                    <ul class="m-0 p-0">
+                        <li>- <a href="#">Lorem ipsum</a></li>
+                        <li>- <a href="#">Nam mauris velit</a></li>
+                        <li>- <a href="#">Etiam vitae mauris</a></li>
+                        <li>- <a href="#">Fusce scelerisque</a></li>
+                        <li>- <a href="#">Sed faucibus</a></li>
+                        <li>- <a href="#">Mauris efficitur nulla</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-xs-12 location">
+                    <h4 class="mt-lg-0 mt-sm-4">Location</h4>
+                    <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
+                    <p class="mb-0"><i class="fa fa-phone mr-3"></i>(541) 754-3010</p>
+                    <p><i class="fa fa-envelope-o mr-3"></i>info@hsdf.com</p>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col copyright">
+                    <p class="">
+                        <small class="text-white-50">Copyright &copy; 2019, Fid-Arc</small>
+                    </p>
+                </div>
             </div>
         </div>
     </footer>
